e87f768a70408fb08c55fc33635baad14333fbe7
talmichaeli <talmich9@gmail.com>
2021-07-05 20:28:25 +0300

    deleted irrelevant code

--- clustering.py
+++ clustering.py
@@ -2,32 +2,6 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
 from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-
-
-def custom_distance(mushroom1, mushroom2):
-    distance_vector = []
-    distance_vector = np.multiply(np.equal(mushroom1, mushroom2), 1) - 1
-    # for category_num in range(np.size(mushroom1)):
-    #     if mushroom1[category_num] == mushroom2[category_num]:
-    #         distance_vector.append(0)
-    #     else:
-    #         distance_vector.append(1)
-    # distance_vector = np.array(distance_vector)
-    return np.linalg.norm(distance_vector)
-
-
-def custom_distance_matrix(x, distance_func):
-    number_of_values = np.shape(x)[0]
-    distance_matrix = np.zeros(shape=(number_of_values, number_of_values))
-    for row1 in range(number_of_values):
-        if row1 % 300 == 0:
-            print(row1)
-        for row2 in range(row1 + 1, number_of_values):
-            distance_matrix[row1, row2] = distance_func(np.array(x[row1, :], dtype=object), np.array(x[row2, :],
-                                                                                                     dtype=object))
-            distance_matrix[row2, row1] = distance_matrix[row1, row2]
-    return distance_matrix
 
 
 def k_means(x):
@@ -46,8 +20,6 @@ def agglomerative_clustering(x):
 
 
 def cluster(data_x):
-    # data_x = np.array(data_x[0:1000, :], dtype=object)
-    # x = custom_distance_matrix(data_x, custom_distance)
     enc = OneHotEncoder()
     enc.fit(data_x)
     x = enc.transform(data_x).toarray()
